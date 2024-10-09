ee5c2719afe5f4107cfd6d4582c798eb9407de63
jyjyjy25 <jyjyjy7418@gmail.com>
2024-10-09 21:51:06 +0900

    Refactor: 기본 root path를 /api/v1으로 변경 (#5)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from routers import user_router, prompt_router
 
-app = FastAPI()
+app = FastAPI(root_path="/api/v1")
 
 app.include_router(user_router.router)
 app.include_router(prompt_router.router)
