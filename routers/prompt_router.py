80323173f07777bbb16ed0ad2a5ac9daec9fa07a
jyjyjy25 <jyjyjy7418@gmail.com>
2024-10-09 21:56:30 +0900

    Refactor: path variable 데이터 타입 변경 (#5)

--- routers/prompt_router.py
+++ routers/prompt_router.py
@@ -6,6 +6,6 @@ from schemas.prompt_schema import PromptChatRequestSchema, PromptChatStreamRespo
 router = APIRouter(prefix="/prompt", tags=["prompt"])
 
 @router.post("/{prompt_id}/chat", response_model=PromptChatStreamResponseSchema)
-async def chat_sse(prompt_id: int, request: PromptChatRequestSchema = Body(...), prompt_service=Depends(prompt_service.PromptService)):
+async def chat_sse(prompt_id: str, request: PromptChatRequestSchema = Body(...), prompt_service=Depends(prompt_service.PromptService)):
     user_input = request.user_input
     return StreamingResponse(prompt_service.handle_chatgpt_conversation(user_input, prompt_id), media_type="text/event-stream")
