a32427835f8fea791be8b585362301c13bbc7c9c
jyjyjy25 <jyjyjy7418@gmail.com>
2024-10-11 21:44:34 +0900

    Feat: 사이즈 5를 갖는 버퍼 생성하여 입력되는 로그 저장 로직 구현 (#6)

--- routers/bert_router.py
+++ routers/bert_router.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter, Depends
 from services.bert_service import BERTService
-from typing import List
+from collections import deque
 
 router = APIRouter(prefix="/bert", tags=["IAMBERT"])
 
+# 로그를 저장할 버퍼 (최대 크기 5)
+log_buffer = deque(maxlen=5)
+
 @router.post("/predict")
-def predict_attack(log_data: List[dict], bert_service: BERTService = Depends(BERTService)):
-    return bert_service.predict_attack(log_data)
+def predict_attack(log_data: dict, bert_service: BERTService = Depends(BERTService)):
+    log_buffer.append(log_data)
+    
+    if len(log_buffer) == 5:
+        return bert_service.predict_attack(list(log_buffer))
