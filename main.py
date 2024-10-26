4e5898ae4f4599831279646e2faf814988ffe646
jyjyjy25 <jyjyjy7418@gmail.com>
2024-10-26 22:22:36 +0900

    Feat: 서버 시작/종료 시 MongoDB 연결 시작/종료 (#15)

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from routers import user_router, prompt_router, bert_router
+from core.mongodb_driver import MongoDB
+from core.logging_config import setup_logger
+
+logger = setup_logger()
 
 app = FastAPI(root_path="/api/v1")
 
@@ -7,6 +11,20 @@ app.include_router(user_router.router)
 app.include_router(prompt_router.router)
 app.include_router(bert_router.router)
 
+mongodb = MongoDB()
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+# 서버 시작 시 MongoDB 연결
+@app.on_event("startup")
+async def startup_db_client():
+    await mongodb.connect()
+    logger.info("MongoDB 연결이 설정되었습니다.")
+
+# 서버 종료 시 MongoDB 연결 종료
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    await mongodb.close()
+    logger.info("MongoDB 연결이 종료되었습니다.")
