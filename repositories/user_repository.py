99c1844e1d286af9ed4874ffe7e812e0db7d392f
jyjyjy25 <jyjyjy7418@gmail.com>
2024-10-27 01:55:54 +0900

    Refactor: mongodb를 사용한 비즈니스 로직을 router, service, repository로 분리 (#15)

--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -2,13 +2,8 @@ from models.user_model import User
 
 class UserRepository:
     def __init__(self):
-        self.users = [
-            User(name="John Doe", age=30, email="johndoe@example.com"),
-            User(name="Jane Doe", age=25, email="janedoe@example.com")
-        ]
+        self.user = User(name="John Doe", age=30, email="johndoe@example.com")  # 테스트 유저 데이터
 
-    def find_by_id(self, user_id: int):
-        for user in self.users:
-            if user.id == user_id:
-                return user
-        return None
+    async def save_user(self, engine):
+        new_user = await engine.save(self.user)
+        return new_user
--- routers/user_router.py
+++ routers/user_router.py
@@ -1,21 +1,11 @@
 from fastapi import APIRouter, Depends
-from services import user_service
-from schemas.user_schema import UserSchema
-from models.user_model import User
-from core.mongodb_driver import MongoDB
+from services.user_service import UserService
+from core.mongodb_driver import mongodb
 
 router = APIRouter(prefix="/users", tags=["users"])
 
-mongodb = MongoDB()
-
 # 사용자 정보를 MongoDB에 저장하는 API
 @router.get("/")
-async def save_user(engine=Depends(mongodb.get_engine)):
-    user = User(name="김지윤", age=24, email="jyjyjy7418@gmail.com")
-    new_user = await engine.save(user)
+async def save_user(user_service: UserService = Depends(), engine=Depends(mongodb.get_engine)):
+    new_user = await user_service.save_user(engine)
     return new_user
-
-# 사용자 정보를 가져오는 API
-@router.get("/{user_id}", response_model=UserSchema)
-def get_user(user_id: int, user_service=Depends(user_service.UserService)):
-    return user_service.get_user_by_id(user_id)
