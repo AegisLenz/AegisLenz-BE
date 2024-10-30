7d94b64638c5d4024e9f6c0a10bcf491db1ab4f0
jyjyjy25 <jyjyjy7418@gmail.com>
2024-10-30 20:40:58 +0900

    Refactor: 응답 로직 함수로 분리 (#23)

--- services/prompt_service.py
+++ services/prompt_service.py
@@ -60,50 +60,37 @@ class PromptService:
             messages=conversation,
             response_format={"type": "json_object"}
         )
-        return response
-
-    async def classify_persona(self, query):
-        response = await self.receive_gpt_response("Classify", query)
         return self.clean_response(response)
 
-    async def dashboard_persona(self, query):      
-        response = await self.receive_gpt_response("DashBoard", query)
-        response = self.clean_response(response)
-        dashboards = json.loads(response).get("selected_dashboards")
-        return dashboards
+    def create_stream_response(self, status="processing", type=None, data=None):
+        response = PromptChatStreamResponseSchema(status=status, type=type, data=data)
+        return json.dumps(response.dict(), ensure_ascii=False) + "\n"
+
+    async def classify_persona(self, query):
+        return await self.receive_gpt_response("Classify", query)
+
+    async def dashboard_persona(self, query):
+        dashboards = await self.receive_gpt_response("DashBoard", query)
+        return json.loads(dashboards).get("selected_dashboards")
 
     async def es_persona(self, query):
-        response = await self.receive_gpt_response("ES", query)
-        
-        es_query = self.clean_response(response)
+        es_query = await self.receive_gpt_response("ES", query)
         if es_query:
             es_result = await self.prompt_repository.find_es_document(es_query)
-            return es_result
+            return es_query, es_result
         else:
             raise HTTPException(status_code=400, detail="Failed ElasticSearch query parsing.")
 
     async def db_persona(self, query):
-        response = await self.receive_gpt_response("DB", query)
-
-        db_query = self.clean_response(response)
+        db_query = await self.receive_gpt_response("DB", query)
         if db_query:
             db_result = await self.prompt_repository.find_db_document(db_query)
-            return db_result
+            return db_query, db_result
         else:
             raise HTTPException(status_code=400, detail="Failed MongoDB query parsing.")
 
     async def policy_persona(self, query):
         return None
-
-    async def generate_response_by_persona(self, classification, query):
-        if classification == "onlyES":
-            persona_response = await self.es_persona(query)
-        elif classification == "onlyMDB":
-            persona_response = await self.db_persona(query)
-        elif classification == "policy":
-            persona_response = await self.policy_persona(query)
-        
-        return persona_response
 
     async def process_prompt(self, user_input, prompt_session_id):
         user_input_content = f"사용자의 자연어 질문: {user_input} 답변은 반드시 json 형식으로 나옵니다."
@@ -121,43 +108,58 @@ class PromptService:
         
         # 대시보드 데이터
         dashboards = await self.dashboard_persona(query)
-        response = PromptChatStreamResponseSchema(status="processing", type="DashBoard", data=dashboards)
-        yield json.dumps(response.dict(), ensure_ascii=False) + "\n"
+        yield self.create_stream_response(type="DashBoard", data=dashboards)
         
         # 분류기 결과에 따른 페르소나 처리
         if classification_result == "DashbPr":
-            persona_response = dashboards
+            pass
         elif classification_result in ["onlyES", "onlyMDB", "policy"]:
-            persona_response = await self.generate_response_by_persona(classification_result, query)
-            persona_response = json.dumps(dashboards) + "\n" + json.dumps(persona_response)
+            if classification_result == "onlyES":
+                es_query, es_result = await self.es_persona(query)
+                persona_response = json.dumps({
+                                                "es_query": es_query,
+                                                "es_result": es_result
+                                            }, ensure_ascii=False)
+                yield self.create_stream_response(type="ESQuery", data=es_query)
+                yield self.create_stream_response(type="ESResult", data=es_result)
+            elif classification_result == "onlyMDB":
+                db_query, db_result = await self.db_persona(query)
+                persona_response = json.dumps({
+                                                "db_query": db_query,
+                                                "db_result": db_result
+                                            }, ensure_ascii=False)
+                yield self.create_stream_response(type="DBQuery", data=db_query)
+                yield self.create_stream_response(type="DBResult", data=db_result)
+            elif classification_result == "policy":
+                await self.policy_persona(query)
+                persona_response = json.dumps()
+
+            logger.info(persona_response)
+            # 요약 데이터
+            stream = self.gpt_client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=[query, {"role": "assistant", "content": persona_response}],
+                stream=True,
+            )
+
+            # 스트리밍 응답 처리
+            assistant_response = ""
+            for chunk in stream:
+                clean_answer = self.clean_streaming_chunk(chunk)
+                if clean_answer:
+                    assistant_response += clean_answer
+                    yield self.create_stream_response(type="Summary", data=clean_answer)
+
+            # 스트리밍이 끝나면 전체 응답을 하나의 문자열로 합쳐 히스토리에 저장
+            conversation_history.append({"role": "assistant", "content": assistant_response, "timestamp": datetime.now(timezone(timedelta(hours=9))).replace(tzinfo=None).isoformat()})
         else:
-            raise HTTPException(status_code=500, detail="Failed classify.")
-
-        # 요약 데이터
-        stream = self.gpt_client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[query, {"role": "assistant", "content": persona_response}],
-            stream=True,
-        )
-
-        # 스트리밍 응답 처리
-        assistant_response = ""
-        for chunk in stream:
-            clean_answer = self.clean_streaming_chunk(chunk)
-            if clean_answer:
-                assistant_response += clean_answer
-                response = PromptChatStreamResponseSchema(status="processing", type="Summary", data=clean_answer)
-                yield json.dumps(response.dict(), ensure_ascii=False) + "\n"
+            raise HTTPException(status_code=500, detail="Failed Streaming.")
         
-        # 스트리밍이 끝나면 전체 응답을 하나의 문자열로 합쳐 히스토리에 저장
-        conversation_history.append({"role": "assistant", "content": assistant_response, "timestamp": datetime.now(timezone(timedelta(hours=9))).replace(tzinfo=None).isoformat()})
-
         # DB에 대화 히스토리 저장
         await self.prompt_repository.save_conversation_history(prompt_session_id, conversation_history)
 
         # 스트리밍 완료 메시지 전송
-        response = PromptChatStreamResponseSchema(status="complete")
-        yield json.dumps(response.dict(), ensure_ascii=False) + "\n"
+        yield self.create_stream_response(status="complete")
 
     async def handle_chatgpt_conversation(self, user_input, prompt_id):
         async for chunk in self.process_prompt(user_input, prompt_id):
