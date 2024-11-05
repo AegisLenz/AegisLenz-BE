106b0658d656cd1015c612fd8fa0395336ff13ad
jyjyjy25 <jyjyjy7418@gmail.com>
2024-11-05 15:40:14 +0900

    Feat: Policy MongoDB에 저장 (#34)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from routers import user_router, prompt_router, bert_router
 from core.mongodb_driver import mongodb
 from core.logging_config import setup_logger
+from utils.insert_initial_data import insert_initial_data
 
 logger = setup_logger()
 
@@ -20,6 +21,7 @@ def read_root():
 async def startup_db_client():
     await mongodb.connect()
     logger.info("MongoDB 연결이 설정되었습니다.")
+    await insert_initial_data()
 
 # 서버 종료 시 MongoDB 연결 종료
 @app.on_event("shutdown")
---
+++ utils/insert_initial_data.py
@@ -0,0 +1,52 @@
+import os
+import json
+from core.mongodb_driver import mongodb
+from models.policy_model import Policy, PolicyAction
+
+async def insert_initial_data():
+    mongodb_engine = mongodb.get_engine()
+
+    # Policy 컬렉션에 데이터가 없으면 초기 데이터를 삽입
+    document_count = await mongodb_engine.count(Policy)
+
+    if document_count == 0:
+        print("컬렉션이 비어 있습니다. 초기 데이터를 삽입합니다.")
+        
+        # 상위 디렉터리의 하위 디렉터리 목록 가져오기
+        base_directory = "/app/AWSDatabase"  # Docker 볼륨에 위치한 AWS 최소권한정책 DB
+        directories = [d for d in os.listdir(base_directory) if os.path.isdir(os.path.join(base_directory, d))]
+
+        # 각 디렉터리의 하위 파일 목록 가져오기 및 데이터 삽입
+        for directory in directories:
+            dir_path = os.path.join(base_directory, directory)
+            files = os.listdir(dir_path)
+            
+            for file in files:
+                file_path = os.path.join(dir_path, file)
+
+                with open(file_path, 'r') as f:
+                    try:
+                        data = json.load(f)
+
+                        policy_actions = [
+                            PolicyAction(
+                                Action=item["Action"],
+                                Effect=item["Effect"],
+                                Resource=item["Resource"]
+                            ) for item in data["policy"]
+                        ]
+                        policy = Policy(
+                            service=directory,
+                            event_name=os.path.splitext(file)[0],
+                            policy=policy_actions
+                        )
+
+                        await mongodb_engine.save(policy)
+                        print(f"{file} 데이터를 MongoDB에 삽입했습니다.")
+            
+                    except json.JSONDecodeError as e:
+                        raise json.JSONDecodeError(f"JSON 디코딩 오류: {file}", file, e.pos)
+                    except KeyError as e:
+                        raise KeyError(f"키 오류: {e} - {file}")
+    else:
+        print("컬렉션에 데이터가 이미 존재합니다. 초기 데이터 삽입을 생략합니다.")
