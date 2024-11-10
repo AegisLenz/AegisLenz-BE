04ffe9d8287322c6d14dec165062831ac96f48d0
jyjyjy25 <jyjyjy7418@gmail.com>
2024-11-11 01:09:17 +0900

    Fix: IAMUser 별 정책 조회 코드 수정 (#40)

--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -1,16 +1,15 @@
-#repositories/user_repository.py
-# 사용자와 자산(UserAsset) 정보를 MongoDB에 저장하고 불러오는 기능
+from fastapi import HTTPException
 from models.user_model import User
-from motor.motor_asyncio import AsyncIOMotorClient
-import asyncio
-from core.mongodb_driver import mongodb #MongoDB와의 연결을 담당하는 mongodb 인스턴스를 가져옴. DB 접속, 저장, 검색 가능
-from models.asset_model import UserAsset # UserAsset 모델. 자산 정보를 mongodb에 저장할 때 사용
+from core.mongodb_driver import mongodb  # MongoDB와의 연결을 담당하는 mongodb 인스턴스를 가져옴. DB 접속, 저장, 검색 가능
+from models.asset_model import UserAsset  # UserAsset 모델. 자산 정보를 mongodb에 저장할 때 사용
 
 
 #사용과와 관련된 데이터베이스 작업을 수행하는 함수를 포함함
 class UserRepository:
     def __init__(self): #UserRepository 클래스의 인스턴스가 생성될 때 자동으로 실행됨
         self.user = User(name="John Doe", age=30, email="johndoe@example.com")  # 테스트 유저 데이터
+        self.mongodb_engine = mongodb.engine
+        self.mongodb_client = mongodb.client
 
     #비동기 함수. mongodb에 사용자를 저장하는 역할
     async def save_user(self, engine):
@@ -33,17 +32,17 @@ class UserRepository:
         else:
             return {"error": "Invalid asset type specified"}
     
-    
     # 사용자의 기존 정책을 가져오는 코드
-    async def get_user_policies(self, user_id: str, engine):
-        # 특정 사용자 ID의 정책을 조회하는 메서드
-        user_asset = await engine.find_one(UserAsset, {"user_id": user_id})
-        if not user_asset or "IAM" not in user_asset.asset:
-            return {"error": "User asset not found or no IAM policies"}
-
-        # 각 IAM 유저의 AttachedPolicies를 추출하여 반환
-        attached_policies = [
-            policy for iam in user_asset.asset["IAM"]
-            for policy in iam.get("AttachedPolicies", [])
-        ]
-        return attached_policies
+    async def get_user_policies(self, user_id: str):
+        try:
+            user_asset = await self.mongodb_engine.find_one(
+                UserAsset,
+                UserAsset.user_id == user_id
+            )
+            # 각 IAM 유저의 AttachedPolicies를 추출하여 반환
+            attached_policies_by_user = {
+                iam.UserName: iam.AttachedPolicies for iam in user_asset.asset.IAM
+            }
+            return attached_policies_by_user
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occurred while fetching messages: {str(e)}")
