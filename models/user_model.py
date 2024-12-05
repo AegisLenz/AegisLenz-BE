f35e05bf0a6f75f64fd36824861dadb7a39d91f2
jyjyjy25 <jyjyjy7418@gmail.com>
2024-12-06 00:13:52 +0900

    Refactor: ‘bookmark’ 필드에 가변 기본값 대신 Field의 default_factory로 변경 (#72)

--- models/user_model.py
+++ models/user_model.py
@@ -7,7 +7,7 @@ from datetime import datetime
 class User(Model):
     id: str = Field(primary_field=True)  # _id를 임시로 문자열로 설정
     email: str
-    bookmark: List[str] = []
+    bookmark: List[str] = Field(default_factory=list)
     
     aws_access_key_id: Optional[str] = None
     aws_secret_access_key: Optional[str] = None
